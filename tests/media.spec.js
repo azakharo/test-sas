dd52cf45009e04c79b7eca95533095aa26f2819e
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2017-10-18 16:11:18 +0300

    media testing impl-ed. NO UPLOAD!

--- tests/media.spec.js
+++ tests/media.spec.js
@@ -9,6 +9,7 @@ const auth = require('./auth');
 describe('media', () => {
 
   let authToken = null;
+  let mediaID = null;
 
   before(() => {
     return auth.login().then((token) => {
@@ -16,9 +17,9 @@ describe('media', () => {
     });
   });
 
-  beforeEach(() => {
-    return console.log('beforeEach');
-  });
+  // IMPORTANT!
+  // Could not upload media!!! This is a BIG problem, still not resolved.
+  // You have to add it manually by hand.
 
   describe('/GET media list', () => {
     it('it should GET all the media files', () => {
@@ -29,87 +30,48 @@ describe('media', () => {
           res.should.have.status(200);
           res.should.be.json;
           res.body.should.be.a('array');
-          // res.body.length.should.be.eql(1);
+          res.body.length.should.be.least(1);
+          mediaID = res.body[0].id;
         });
     });
   });
-  // describe('/POST book', () => {
-  //   it('it should not POST a thing without name field', () => {
-  //     return chai.request(server)
-  //       .post('/api/things')
-  //       .set('Authorization', `Bearer ${authToken}`)
-  //       .send({})
-  //       .catch((err) => {
-  //         err.should.have.status(400);
-  //         err.should.have.property('response');
-  //         err.response.should.have.property('text');
-  //         err.response.text.should.be.a('string');
-  //         err.response.text.should.eql('name is missing');
-  //       });
-  //   });
-  //   it('it should POST a thing', () => {
-  //     const thing = {
-  //       name: 'a thing'
-  //     };
-  //     return chai.request(server)
-  //       .post('/api/things')
-  //       .set('Authorization', `Bearer ${authToken}`)
-  //       .send(thing)
-  //       .then((res) => {
-  //         res.should.have.status(201);
-  //         res.body.should.be.an('object');
-  //         res.body.should.have.property('name');
-  //       });
-  //   });
-  // });
-  // describe('/GET/:id thing', () => {
-  //   it('it should GET a thing by the given id', () => {
-  //     const thing = new Thing({name: 'a new thing'});
-  //     return thing.save()
-  //       .then(savedThing => {
-  //         return chai.request(server)
-  //           .get('/api/things/' + savedThing._id)
-  //           .then(res => {
-  //             res.should.have.status(200);
-  //             res.body.should.be.a('object');
-  //             res.body.should.have.property('name');
-  //             res.body.name.should.equal('a new thing');
-  //           });
-  //       });
-  //   });
-  // });
-  // describe('/PUT/:id thing', () => {
-  //   it('it should UPDATE a thing given the id', () => {
-  //     const thing = new Thing({name: 'a new thing'});
-  //     return thing.save()
-  //       .then(thingCreated => {
-  //         return chai.request(server)
-  //           .put('/api/things/' + thingCreated._id)
-  //           .set('Authorization', `Bearer ${authToken}`)
-  //           .send({name: 'modified thing'})
-  //           .then(res => {
-  //             res.should.have.status(200);
-  //             res.body.should.be.a('object');
-  //             res.body.should.have.property('name').equal('modified thing');
-  //           });
-  //     });
-  //   });
-  // });
-  // describe('/DELETE/:id thing', () => {
-  //   it('it should DELETE a thing given the id', () => {
-  //     const thing = new Thing({name: 'a new thing'});
-  //     return thing.save()
-  //       .then(newThing => {
-  //         return chai.request(server)
-  //           .delete('/api/things/' + newThing._id)
-  //           .set('Authorization', `Bearer ${authToken}`)
-  //           .then(res => {
-  //             res.should.have.status(200);
-  //             res.should.have.property('text');
-  //             res.text.should.be.a('string');
-  //             res.text.should.equal('OK');
-  //           });
-  //       });
-  //   });
-  // });
+
+  describe('/GET media description', () => {
+    it('it should GET media description by the given id', () => {
+      return chai.request(server)
+        .get('/api/media/' + mediaID + '.json')
+        .set('Authorization', `Bearer ${authToken}`)
+        .then(res => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('id');
+          res.body.should.have.property('date_created');
+          res.body.should.have.property('creator');
+          res.body.should.have.property('content-type');
+          res.body.should.have.property('title');
+        });
+    });
+  });
+
+  describe('/GET media file content', () => {
+    it('it should GET media content by the given id', () => {
+      return chai.request(server)
+        .get('/api/media/' + mediaID)
+        .set('Authorization', `Bearer ${authToken}`)
+        .then(res => {
+          res.should.have.status(200);
+        });
+    });
+  });
+
+  describe('/DELETE media', () => {
+    it('it should DELETE media by the given id', () => {
+      return chai.request(server)
+        .delete('/api/media/' + mediaID)
+        .set('Authorization', `Bearer ${authToken}`)
+        .then(res => {
+          res.should.have.status(200);
+        });
+    });
+  });
 });
